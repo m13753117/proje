c9f215bc40dea08e3bcd93c6c1d8a283fdc7908a
m13753117 <farid020ibraah@gmail.com>
2025-05-03 12:59:50 +0200

    Add files via upload

---
+++ composant/footer.php
@@ -0,0 +1,45 @@
+<div class="foo">
+    <div class="col-lg-4">
+
+      <div class="social">
+        <ul>
+          <li><a href="#" ><img src="IMG/fb.png"><span>Facebook</span></a></li>
+          <li><a href="#" ><img src="IMG/go.png"><span>Google+</span></a></li>
+          <li><a href="#" ><img src="IMG/tw.png"><span>Twitter</span></a></li>
+          <li><a href="#" ><img src="IMG/yt.png"><span>Youtube</span></a></li>
+        </ul>
+      </div>
+
+    </div>
+
+    <div class="col-lg-4">
+      <div class="propos">
+        <h2 class="foo-headline">A propos & Administration des Emprunts</h2>
+        <p>Ce site a ete concu dans le cadre des mini projects asigner aux etudiant.<br><a href="admin/index.php">Administration</a>
+      </p>
+        
+      </div>
+    </div>
+    
+    <div class="col-lg-4">
+
+      <div class="team">        
+        <ul>
+          <h2 class="foo-headline">Ce site est creer par:</h2>
+
+          <li><a herf="#">Altine Sanda Ismael</a></li>
+        
+        </ul>
+      </div>  
+
+    </div>
+    
+
+</div>
+<div class="rights" >
+  © 2025 copyrights to <span class="italic">NGR</span>
+</div>
+
+
+<script type="text/javascript" src="CSS/bootstrap/js/bootstrap.min.js">
+</script>
